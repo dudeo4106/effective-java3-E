3ad3d86c2f959fe209ff3c49ddc4231f4d7d0bd5
djung <dudeo4106@gmail.com>
2021-09-16 23:04:52 +0900

    add item02

--- src/main/java/chapter02/item02/Main.java
+++ src/main/java/chapter02/item02/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         NyPizza nyPizza = new NyPizza.Builder(NyPizza.Size.SMALL)
                 .addTopping(Pizza.Topping.SAUSAGE)
-                .addTopping(Pizza.Topping.ONION)
+                .addTopping(Pizza.Topping.PEEPER)
+                .build();
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM)
+                .sauceInside()
                 .build();
     }
 }
